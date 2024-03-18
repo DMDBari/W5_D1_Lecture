8e2f0058e11fa2726b617ab2541c74a7992ed46a
Dillon Dains <dilloncsbb@gmail.com>
2024-03-18 14:23:54 -0500

    initial commit

---
+++ whiteboard.py
@@ -0,0 +1,19 @@
+# You need to write a function, that returns the first non-repeated character in the given string.
+
+# If all the characters are unique, return the first character of the string.
+# If there is no unique character, return None.
+
+# You can assume, that the input string has always non-zero length.
+
+# Examples
+# "test"   returns "e"
+# "teeter" returns "r"
+# "trend"  returns "t" (all the characters are unique)
+# "aabbcc" returns None (all the characters are repeated)
+
+def solution(string):
+    for char in string:
+        if string.count(char) == 1:
+            return char
+    return None
+
